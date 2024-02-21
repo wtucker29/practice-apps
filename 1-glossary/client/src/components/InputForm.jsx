6224685aed7b4cc6308c88116e2ad280fe8928b3
William Tucker <william.tucker29@gmail.com>
2024-02-21 13:53:41 -0600

    Created InputForm component. This holds a form element with an input box for a word and another for a definition. The form also includes a submit button. Created functions to handle click events and word/definition input changes.

---
+++ 1-glossary/client/src/components/InputForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+const { useState } = React;
+
+var InputForm = ({ onInputClick }) => {
+  const [inputWord, setInputWord] = useState('');
+  const [inputDef, setInputDef] = useState('');
+
+  // functions to deal with adding word and def
+  const handleWordChange = (event) => {
+    // something
+    var newWord = event.target.value;
+    setInputWord(newWord);
+  };
+
+  const handleDefChange = (event) => {
+    // something
+    var newDef = event.target.value;
+    setInputDef(newDef);
+  };
+
+  const handleInputClick = () => {
+    // something
+    onInputClick(inputWord, inputDef);
+  };
+
+  return (
+    <form>
+      <div className="word-div"><b>Word:</b>
+        <input className="input-word" type="text" onChange={handleWordChange}/>
+      </div>
+      <div className="def-div"><b>Defintion:</b>
+        <input className="input-def" type="text" onChange={handleDefChange} />
+        <button className="btn-input" onClick={handleInputClick}>
+          <span className="span-input">Submit</span>
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default InputForm;
