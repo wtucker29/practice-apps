9fb909f629ccf24c4266cd68890ca1b3b9c53616
William Tucker <william.tucker29@gmail.com>
2024-02-21 13:50:04 -0600

    Established connection to MongoDB using mongoose and .env file. Created schema. Set schema to variable. Created save function that saves each post request data as a new entry into the database. Exported both models.

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,27 @@
 const mongoose = require("mongoose");
 
 // 1. Use mongoose to establish a connection to MongoDB
+console.log(`mongodb://localhost/${process.env.DB_NAME}`);
+mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // 2. Set up any schema and models needed by the app
+let glossSchema = mongoose.Schema({
+  word: String,
+  definition: String
+});
+
+let Gloss = mongoose.model('Gloss', glossSchema);
+
+// Save function to save glossary information to mongoDB
+let save = (glossaryData) => {
+  // return Promise.all(glossaryData.map(glossary => {
+  //   return new Gloss(glossary).save();
+  // }))
+  return new Gloss(glossaryData).save();
+}
+
 // 3. Export the models
+module.exports.Gloss = Gloss;
+module.exports.save = save;
+
 // 4. Import the models into any modules that need them
