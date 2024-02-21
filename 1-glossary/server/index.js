727362cc557f05d1540be3144e1d749fcc18c4d1
William Tucker <william.tucker29@gmail.com>
2024-02-21 13:48:51 -0600

    Created express server for application. Installed morgan and cors middleware. Used middleware and express.json. Imported db.js file. Created POST and GET requests. Tested with Postman and both requests working as intended.

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,55 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const morgan = require('morgan');
+const cors = require('cors');
+const db = require('./db.js');
 
 const app = express();
 
-// Serves up all static and generated assets in in a specified folder.
-app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
 
-/****
- *
- *
- * Other routes here....
- *
- *
- */
+// Serves up all static and generated assets in in a specified folder.
+app.use(express.static(path.join(__dirname, '..','client', 'dist')));
+
+// Other routes here
+
+app.post('/glossary', function(req, res) {
+  // Code here
+  // console.log('req.body: ', req.body);
+  // const { word, definition } = req.body;
+  // console.log('word from req.body: ', word);
+  // console.log('def from req.body: ', definition);
+  db.save(req.body)
+    .then(() => {
+      return db.Gloss.find({});
+    })
+    .then((updatedGlossary) => {
+      res.status(201).send(updatedGlossary);
+    })
+    .catch((error) => {
+      console.error('Error saving glossary entry:', error);
+      res.status(500).send('Error saving glosssary');
+    });
+});
+
+app.get('/glossary', function(req, res) {
+  // Code here
+  db.Gloss.find({})
+    .then((entries) => {
+      res.status(200).send(entries);
+    })
+    .catch((error) => {
+      console.error('Error retrivieving glossary entries:', error);
+      res.status(500).send('Internal server error');
+    })
+});
+
+
+
+
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
