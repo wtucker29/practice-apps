aca96a5d5b8ea9e43b54c285f5d4a0a0ae1f73c9
William Tucker <william.tucker29@gmail.com>
2024-02-21 21:02:30 -0600

    Added html elements for Update, Delete, Confirm, and Cancel buttons. Created functions to handle click events for all of those buttons.

--- 1-glossary/client/src/components/GlossaryListEntry.jsx
+++ 1-glossary/client/src/components/GlossaryListEntry.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
 const { useState } = React;
 
-var GlossaryListEntry = ({ glossary }) => {
+var GlossaryListEntry = ({ glossary, onDeleteClick, onUpdateClick }) => {
+  console.log(glossary);
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedDefintion, setUpdatedDefinition] = useState(glossary.definition);
+
+  const handleUpdateClick = () => {
+    setIsEditing(true);
+  }
+
+  const handleInputChange = (event) => {
+    setUpdatedDefinition(event.target.value);
+  }
+
+  const handleUpdateConfirm = () => {
+    const word = glossary.word;
+    const def = updatedDefintion;
+    onUpdateClick(word, def);
+    setIsEditing(false);
+  }
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+    setUpdatedDefinition(glossary.definition);
+  }
+
+  const handleDeleteClick = () => {
+    const word = glossary.word;
+    const def = glossary.definition;
+    console.log('word in deleteclick: ', word);
+    console.log('def in deleteclick: ', def);
+    onDeleteClick(word, def);
+  }
+
   return (
     <ul>
-      <li className="glossary-word">{glossary.word}</li>
-      <div className="glossary-definition">{glossary.defintion}</div>
+      <div>
+        <li className="glossary-word"><b>{glossary.word}:</b> {glossary.definition}</li>
+        {!isEditing && (
+          <button className="btn-update" onClick={handleUpdateClick}>Update</button>
+        )}
+        <button className="btn-delete" onClick={handleDeleteClick}>Delete</button>
+      </div>
+      {isEditing && (
+        <div>
+          <input type="text" value={updatedDefintion} onChange={handleInputChange} />
+          <button onClick={handleUpdateConfirm}>Confirm</button>
+          <button onClick={handleCancelClick}>Cancel</button>
+        </div>
+      )}
     </ul>
   )
 }
