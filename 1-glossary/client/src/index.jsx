da84965b4afb60c906873eb66548b383db8ee2d1
William Tucker <william.tucker29@gmail.com>
2024-02-21 21:05:09 -0600

    Created functions to handle click events. Wrote axios requests within them.

--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -25,14 +25,94 @@ const App = () => {
   }, []);
 
   const handleInputClick = (word, def) => {
-    const addedGlossInput = {word: word, defintion: def};
+    const addedGlossInput = {word: word, definition: def};
+    console.log(addedGlossInput);
 
-    // axios.post('http://localhost:3000/glossary', addedGlossInput)
+    axios.post('http://localhost:3000/glossary', addedGlossInput)
+      .then((response) => {
+        console.log('Response from post request: ', response.data);
+      })
+      .catch((error) => {
+        console.error('Error adding word and definition to glossary:', error);
+      })
+      .then(() => {
+        axios.get('http://localhost:3000/glossary')
+          .then((response) => {
+            console.log('Response from get request: ', response.data);
+            setGlossaryList(response.data);
+            setFilteredList(response.data);
+          })
+          .catch((error) => {
+            console.error('Error loading glossary:', error);
+          });
+      });
   };
 
   const handleSearchClick = (query) => {
     // something here
-    // axios.get('http://localhost:3000/glossary)
+    const searchedWord = { word: query };
+    const filteredWord = glossaryList.filter(entry => entry.word.includes(query));
+    setFilteredList(filteredWord);
+
+    // axios.get(`http://localhost:3000/glossary/${query}`)
+    //   .then((response) => {
+    //     console.log(response);
+    //     console.log('Response from search GET request: ', response.data);
+    //     setFilteredList(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error retrieving searched word:', error);
+    //   });
+  }
+
+  const handleResetClick = () => {
+    //something here
+    setFilteredList(glossaryList);
+  }
+
+  const handleDeleteClick = (word, def) => {
+    const itemToDelete = { word: word, definition: def };
+    axios.delete('http://localhost:3000/glossary', { data: itemToDelete })
+      .then((response) => {
+        console.log('Response from delete request: ', response.data);
+      })
+      .catch((error) => {
+        console.error('Error deleting word and definition:', error);
+      })
+      .then(() => {
+        axios.get('http://localhost:3000/glossary')
+          .then((response) => {
+            console.log('Response from get request: ', response.data);
+            setGlossaryList(response.data);
+            setFilteredList(response.data);
+          })
+          .catch((error) => {
+            console.error('Error loading glossary:', error);
+          });
+      });
+  }
+
+  const handleUpdateClick = (word, def) => {
+    // something
+    const itemToUpdate = { word: word, definition: def };
+    axios.put(`http://localhost:3000/glossary/${word}`, itemToUpdate)
+      .then((response) => {
+        console.log('Response from put request: ', response.data);
+      })
+      .catch((error) => {
+        console.error('Error updating definition:', error);
+      })
+      .then(() => {
+        axios.get('http://localhost:3000/glossary')
+          .then((response) => {
+            console.log('Response from get request: ', response.data);
+            setGlossaryList(response.data);
+            setFilteredList(response.data);
+          })
+          .catch((error) => {
+            console.error('Error loading glossary:', error);
+          });
+      });
   }
 
   return (
@@ -42,10 +122,10 @@ const App = () => {
         <InputForm onInputClick={handleInputClick} />
       </div>
       <div className="searchbar">
-        <Search onSearchClick={handleSearchClick} />
+        <Search onSearchClick={handleSearchClick} onResetClick={handleResetClick} />
       </div>
       <div className="list">
-        <GlossaryList glossaries={glossaryList} filtered={filteredList} />
+        <GlossaryList glossaries={filteredList} onDeleteClick={handleDeleteClick} onUpdateClick={handleUpdateClick} />
       </div>
     </div>
   );
