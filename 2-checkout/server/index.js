e8eddc526f90566ca421d8c6bc0bee16dfebd79f
William Tucker <william.tucker29@gmail.com>
2024-02-22 19:02:11 -0600

    Created file to start server. Holds my controllers functions. Get/Post/Put requests here.

--- 2-checkout/server/index.js
+++ 2-checkout/server/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const sessionHandler = require("./middleware/session-handler");
-const logger = require("./middleware/logger");
+const sessionHandler = require("./middleware/session-handler.js");
+const logger = require("./middleware/logger.js");
 
 // Establishes connection to the database on server start
-const db = require("./db");
+const { db, getInfo, postInfo, putInfo } = require("./db");
 
 const app = express();
+app.use(express.json());
 
 // Adds `req.session_id` based on the incoming cookie value.
 // Generates a new session if one does not exist.
@@ -17,7 +18,7 @@ app.use(sessionHandler);
 app.use(logger);
 
 // Serves up all static and generated assets in a specified folder.
-app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 /****
  *
@@ -26,6 +27,47 @@ app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
  *
  *
  */
+
+app.get('/checkout', function(req, res) {
+
+  const sessionid = req.session_id;
+  console.log(sessionid);
+  getInfo(sessionid, (err, result) => {
+    if (err) {
+      res.status(500).send('Error retrieving checkout information');
+      return;
+    }
+    res.status(200).send(result);
+  })
+});
+
+app.post('/checkout', function(req, res) {
+
+  const sessionid = req.session_id;
+  const { name, email, password } = req.body;
+  const accountData = { sessionid, name, email, password };
+  postInfo(accountData, (err, result) => {
+    if (err) {
+      res.status(500).send('Error sending information');
+      return;
+    }
+    res.status(201).send(result);
+  });
+
+});
+
+app.put('/checkout', function(req, res) {
+  // hardcoding sessionid for postman tests
+  const sessionid = req.session_id;
+  console.log('sessionid in server put request: ', sessionid);
+  putInfo(sessionid, req.body, (err, result) => {
+    if (err) {
+      res.status(500).send('Error updating information');
+      return;
+    }
+    res.status(201).send(result);
+  })
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
