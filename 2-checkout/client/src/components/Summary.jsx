d85c4e252219cda65b14486371f0ebba5278997d
William Tucker <william.tucker29@gmail.com>
2024-02-22 19:00:56 -0600

    Displays Summary of all information added. Currently working on having the data displayed in the element. Testing now.

---
+++ 2-checkout/client/src/components/Summary.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+const { useState } = React;
+
+var Summary = ({ totalSummary, onPurchaseClick }) => {
+
+  const handlePurchaseClick = () => {
+    var number = 0;
+    onPurchaseClick(number);
+  }
+
+
+  return (
+    <div>
+      <h3>Order Summary:</h3>
+      <div className="summary-div">
+        <ul>
+          <li><b>Name:</b> {totalSummary.name}</li>
+          <li><b>Email:</b> {totalSummary.email}</li>
+          <li><b>Password:</b> {totalSummary.password}</li>
+          <li><b>Address Line 1:</b> {totalSummary.addressline1}</li>
+          <li><b>Address Line 2:</b> {totalSummary.addressline2}</li>
+          <li><b>City:</b> {totalSummary.city}</li>
+          <li><b>State:</b> {totalSummary.state}</li>
+          <li><b>Zipcode:</b> {totalSummary.zipcode}</li>
+          <li><b>Credit Card Number:</b> {totalSummary.creditcardnumber}</li>
+          <li><b>Expiry Date:</b> {totalSummary.expirydate}</li>
+          <li><b>CVV:</b> {totalSummary.cvv}</li>
+          <li><b>Billing Zipcode:</b> {totalSummary.billingzip}</li>
+        </ul>
+      </div>
+      <button className="btn-purchase" onClick={handlePurchaseClick}>
+        <span>Purchase</span>
+      </button>
+    </div>
+  )
+}
+
+export default Summary;
